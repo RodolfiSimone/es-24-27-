7a0b32e0f59f4eace9e4616567ccc791914213ee
unknown <rodolfi.simone05@gmail.com>
2020-12-02 17:43:58 +0100

    aggìunto ultimi 4 es

---
+++ es 28.py
@@ -0,0 +1,10 @@
+nomi = input("Nomi di ogni partencipante separati da uno spazio: ").split()
+punteggi = []
+for i in range(len(nomi)):
+    punteggio = int(input("Il punteggio del partencipante corrispondente: "))
+    punteggi.append(punteggio)
+
+punteggio_max = max(punteggi)
+indice = int(punteggi.index(punteggio_max))
+vincitore = nomi[indice]
+print("Il vincitore è : ", vincitore,"con", punteggio_max, "punti")
---
+++ es 31.py
@@ -0,0 +1,12 @@
+numero_decimale = int(input("Inserire numero decimale "))
+resto = 0
+numero_binario = ""
+numero_binario2 = bin(numero_decimale)
+while True:
+    if numero_decimale == 0:
+        break
+    resto = str(numero_decimale%2)
+    numero_decimale = int(numero_decimale / 2)
+    numero_binario = resto + numero_binario
+
+print ("Ecco il confronto dei risultati di due metodi per trasformare decimale in binario",numero_binario,"e", numero_binario2)
